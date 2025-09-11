fb64432d5660cbef8e67be21686de3e02eefe0d3
Kurniawan <hartadichuadri15@gmail.com>
2025-09-11 16:14:06 +1000

    changed to 0.0.0.0 from localhost

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ const start = async () => {
     const app: Express = createApp();
     initializeMonthlyBalanceCheck();
     server = app.listen(PORT, '0.0.0.0',() => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
       console.log(`📘 Swagger docs at http://localhost:${PORT}/api-docs`);
     });
   } catch (err) {
